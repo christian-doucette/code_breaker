4c16c8a6da98457433629cefb702099f6ebcaea5
Christian Doucette <cdoucette@uchicago.edu>
2021-03-27 01:53:01 -0500

    reverted to beam search since its much faster, also improved by decreasing the smoothing constant

--- test_n_gram.py
+++ test_n_gram.py
@@ -1,7 +1,5 @@
 import math
 import json
-import random
-
 import encrypt
 
 
@@ -29,7 +27,7 @@ def calculate_score(text: str, n: int):
 
         for i in range(0, len(word) + n - 1):
             this_ids = str(tuple(letter_ids[i:i+5]))
-            perplexity_log += math.log(n_grams.get(this_ids, 0) + 1, 10)
+            perplexity_log += math.log(n_grams.get(this_ids, 0) + .000001, 10)
             #print(f'Checking {i} to {i+5} in {word}: {this_ids}')
 
     return perplexity_log
@@ -56,40 +54,52 @@ def break_caesar(cipher_text: str, n: int) -> str:
 
 
 
+# helper function for break_substitution
+def get_ext_order(cipher_text: str, n: int):
+        # ext_order is the order in which letter substitutions will be guessed,
+        # for now just sorting by letter frequency in ciphertext
+
+        ext_order = []
+        while len(ext_order) < 26:
+            # greedily chooses letter in most valid n-grams at each step
+            max_letter = ""
+            max_letter_num_grams = -1
+
+            for alphabet_letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+                if alphabet_letter not in ext_order:
+                    num_grams_for_this_letter = 0
+
+                    text_words = cipher_text.split()
+                    for word in text_words:
+                        letter_ids = to_ids_uppercase(word, n)
+                        for i in range(0, len(word) + n - 1):
+                            this_ids = tuple(letter_ids[i:i+5])
+                            is_valid_gram = True
+                            for id in this_ids:
+                                if id not in [ord(ext_letter) - 65 for ext_letter in ext_order] and id != 26 and id != 27 and id != ord(alphabet_letter) - 65:
+                                     is_valid_gram = False
+
+                            if is_valid_gram:
+                                num_grams_for_this_letter += 1
+
+
+
+
+                    if max_letter_num_grams < num_grams_for_this_letter:
+                        max_letter_num_grams = num_grams_for_this_letter
+                        max_letter           = alphabet_letter
+
+
+            ext_order.append(max_letter)
+
+        return ext_order
+
 
 # implentation of beam search algorithm from this paper: https://www.aclweb.org/anthology/P13-1154.pdf
 # assumes text only includes uppercase letters and spaces
-def break_substitution(cipher_text: str, num_trials: int = 15, n_swaps: int = 1000) -> str:
+def break_substitution(cipher_text: str, n: int, n_keep: int = 50) -> str:
     # number of letters in the guesses so far
-    best_score_overall = -1
-    best_score_func    = {letter: letter for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
-
-    for trial_num in range(num_trials):
-        print(f'Trial {trial_num}')
-        this_trial_best_score = -1
-        this_trial_func       = encrypt.get_random_substitution()
-
-
-        for _ in range(n_swaps):
-            swap = random.sample(range(26), k=2)
-            swapped_func = this_trial_func.copy()
-            swapped_func[chr(swap[0] + 65)] = this_trial_func[chr(swap[1] + 65)]
-            swapped_func[chr(swap[1] + 65)] = this_trial_func[chr(swap[0] + 65)]
-
-            swapped_func_score = calculate_score(encrypt.encrypt_substitution(cipher_text, swapped_func), 5)
-            if swapped_func_score > this_trial_best_score:
-                this_trial_func          = swapped_func
-                this_trial_best_score    = swapped_func_score
-
-        if this_trial_best_score > best_score_overall:
-            best_score_overall = this_trial_best_score
-            best_score_func    = this_trial_func
-
-    return encrypt.encrypt_substitution(cipher_text, best_score_func)
-
-
-
-
+    cardinality = 0
 
     # lists, each entry is a tuple of (partial decipherment function, score for that function)
     H_s = [(dict(), 0)]
@@ -157,8 +167,10 @@ n_val = len(eval(next(iter(n_grams))))
 print(f'n_val: {n_val}')
 
 
-my_cipher_text = encrypt.encrypt_substitution("MY NAME IS CHRISTIAN DOUCETTE AND I APPROVE THIS MESSAGE", encrypt.get_random_substitution())
-#print(my_cipher_text)
-print(break_substitution("PA QB EMCO HAF RMLB PA RMLB M LBOH RCVR CS PA FUWBODPMUW OCNX MUW ZAOPH PRB RFZAO CD BKPOBZBYH DFQPYB MUW GCPRAFP M DAYCW VOMDI AE PRBAOBPCNMY IRHDCND ZADP AE PRB TAXBD GCYY VA ALBO M PHICNMY LCBGBOD RBMW PRBOBD MYDA OCNXD UCRCYCDPCN AFPYAAX GRCNR CD WBEPYH GALBU CUPA RCD NRMOMNPBOCDMPCAU  RCD IBODAUMY IRCYADAIRH WOMGD RBMLCYH EOAZ UMOAWUMHM LAYHM YCPBOMPFOB EAO CUDPMUNB PRB EMUD FUWBODPMUW PRCD DPFEE PRBH RMLB PRB CUPBYYBNPFMY NMIMNCPH PA POFYH MIIOBNCMPB PRB WBIPRD AE PRBDB TAXBD PA OBMYCJB PRMP PRBHOB UAP TFDP EFUUH PRBH DMH DAZBPRCUV WBBI MQAFP YCEB MD M NAUDBSFBUNB IBAIYB GRA WCDYCXB OCNX MUW ZAOPH POFYH MOB CWCAPD AE NAFODB PRBH GAFYWUP MIIOBNCMPB EAO CUDPMUNB PRB RFZAFO CU OCNXD BKCDPBUNCMY NMPNRIROMDB GFQQM YFQQM WFQ WFQ GRCNR CPDBYE CD M NOHIPCN OBEBOBUNB PA PFOVBUBLD OFDDCMU BICN EMPRBOD MUW DAUD CZ DZCOXCUV OCVRP UAG TFDP CZMVCUCUV AUB AE PRADB MWWYBIMPBW DCZIYBPAUD DNOMPNRCUV PRBCO RBMWD CU NAUEFDCAU MD WMU RMOZAUD VBUCFD FUEAYWD CPDBYE AU PRBCO PBYBLCDCAU DNOBBUD GRMP EAAYD RAG C ICPH PRBZ MUW HBD QH PRB GMH C WA RMLB M OCNX MUW ZAOPH PMPPAA MUW UA HAF NMUUAP DBB CP CPD EAO PRB YMWCBD BHBD AUYH MUW BLBU PRBH RMLB PA WBZAUDPOMPB PRMP PRBHOB GCPRCU CS IACUPD AE ZH AGU IOBEBOMQYH YAGBO QBEAOBRMUW"))
+my_cipher_text = encrypt.encrypt_substitution("THE MOST FUN I COULD HAVE WOULD BE TO JUST ENJOY MYSELF IN THE SUMMER SUN WITH A COOL GLASS OF WATER IN MY HAND", encrypt.get_random_substitution())
+print(my_cipher_text)
+#print(break_substitution("AIQ UZT EIV YVQM TVA XQZY KVVDM IZM TV ZYOZTAZNQ VOQX AIQ UZT AIZA LZT TVA XQZY AIQU", 5))
+print(break_substitution(my_cipher_text, 5))
+
 # Tests it by attempting to break a caesar cipher
 # print(f'BEST GUESS: {break_caesar("QFC QYYYYYYYYYYGB", n_val)}')
